02cdd1eaf846d88bc16c35fcf6cddfd17781127c
SanketTops <sanket.tops@gmail.com>
2026-02-06 10:27:31 +0530

    Module-1

---
+++ Assignments/Module-1/print.py
@@ -0,0 +1,8 @@
+'''print('Hello Students!')
+print("Hello Students!")'''
+
+print('''Good Morning!
+      rheiwriworoiwoeitr
+      roiewoiroiwoirpwr
+      rowerpow''')
+
---
+++ Assignments/Module-1/sep.py
@@ -0,0 +1,1 @@
+print("Hi","Hello","Good Morning!",sep=",")
