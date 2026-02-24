b87e94eb6a1035c0b4a3fa54839ef04a9d26137f
SanketTops <sanket.tops@gmail.com>
2026-02-24 10:53:03 +0530

    String

---
+++ Assignments/Module-1/str_example.py
@@ -0,0 +1,33 @@
+mystr="this is python!"
+"""print(mystr)
+print(mystr[0])
+print(mystr[-1])
+print(mystr[0:4]) #range - slice
+print(mystr[2:])
+print(mystr[:4])"""
+
+# -------------------------- #
+"""print(mystr)
+print(len(mystr))"""
+
+# ------------ String Methods ------------ #
+print(mystr)
+#print(mystr.strip())    
+#print(mystr.lower())
+#print(mystr.upper())
+#print(mystr.replace('This','That'))
+#print(mystr.replace('T','Y'))
+#print(mystr.split())
+#print(mystr.capitalize())
+#print(mystr.casefold())
+#print(mystr.count("IS"))
+#print(mystr.endswith("!"))
+#print(mystr.startswith("t"))
+#print(mystr.find("P"))
+#print(mystr.index("P"))
+#print(mystr.isalpha())
+#print(mystr.isdigit())
+#print(mystr.isalnum())
+#print(mystr.islower())
+#print(mystr.isupper())
+print(mystr.title())
