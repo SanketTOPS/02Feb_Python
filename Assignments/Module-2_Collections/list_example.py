b17607205e2716a609842a7fe570b8c5fbff11dd
SanketTops <sanket.tops@gmail.com>
2026-02-25 10:52:17 +0530

    List

---
+++ Assignments/Module-2_Collections/list_example.py
@@ -0,0 +1,48 @@
+data=["python","java","php",".net","html","java"]
+"""print(data)
+print(data[0])
+print(data[-1])
+print(data[0:3]) #range - slice
+print(data[1:])
+print(len(data))
+"""
+
+# ----------------------------- #
+#print(data)
+
+"""if 'java' in data:
+    print("Yes...")
+else:
+    print("No..")"""
+    
+#print(data)
+
+"""for i in data:
+    print(i)"""
+
+#data[1]="angular"
+#print(data)
+
+# --------- List's Methods --------- #
+print(data)
+#data.append("react")
+#data.insert(3,'css')
+#data.remove('.net')
+#data.pop()
+#data.pop(1)
+#data.clear()
+#data.reverse()
+#data.sort()
+#del data
+#print(data.index("php"))
+#print(data)
+
+#newdata=data.copy()
+#print(newdata)
+
+newdata=["c","c++","ds","dsa"]
+print(newdata)
+
+#print(data+newdata)
+data.extend(newdata)
+print(data)
