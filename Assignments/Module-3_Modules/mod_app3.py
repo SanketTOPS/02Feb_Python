5f8ff366576567c6f859f3c667afce13284699d1
SanketTops <sanket.tops@gmail.com>
2026-03-10 10:59:06 +0530

    Modules

---
+++ Assignments/Module-3_Modules/mod_app3.py
@@ -0,0 +1,5 @@
+from tops import *
+
+getsum(12,43)
+production(34,65)
+getdata(101,'Sanket')
---
+++ Assignments/Module-3_Modules/random_lib.py
@@ -0,0 +1,13 @@
+import random
+
+#x=random.random()
+#x=random.randint(1111,9999)
+#x=random.randrange(111111,999999,510)
+#print(x)
+
+# --------------------- #
+captcha=['fh7T','wkx8','Iwn5','Ixmn','UYw3','POcm','Own4','Ocm3']
+#x=random.choice(captcha)
+#print(x)
+random.shuffle(captcha)
+print(captcha)
