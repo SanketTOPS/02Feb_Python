ce13d3f210c427870d382042d8d47c980ca2eac8
SanketTops <sanket.tops@gmail.com>
2026-05-23 12:02:01 +0530

    API

---
+++ Projects/APIProject/apiapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Create your views here.
+
+@api_view(['GET'])
+def getdata(request):
+    stdata=Studinfo.objects.all()
+    serial=StudSerial(stdata,many=True)
+    return Response(serial.data)
+    
